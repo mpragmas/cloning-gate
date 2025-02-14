bf0cf783cbd3f3da3acd5705d2530ba2c7c4b0bf
mpragmas <mpragmas@gmail.com>
2025-02-14 21:12:49 +0200

    Adding section

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    extend: {
+      backgroundImage: {
+        birds: "url('/img/many-birds.jpeg')",
+      },
+    },
+  },
+  plugins: [],
+};
